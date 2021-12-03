1b021a7c2f3c0163e02724bf01815f42fb7227c9
CQFitz <nnabilmuqri123@gmail.com>
2021-12-03 15:17:06 +0800

    process

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ import flask
 
 from config import app_config
 from .views import register_views
+from  sqlalchemy.sql.expression import func, select
 
 def create_app(config_name):
     app = flask.Flask(__name__, instance_relative_config=True)
@@ -22,6 +23,10 @@ def create_app(config_name):
 
     @app.route('/')
     def botanic_garden():
-        return flask.render_template('site.html', app=app)
+        random_plant = models.Plants.query.order_by(func.random()).first()
+        get_family = models.Famillies.by_id(random_plant.family_id)
+        get_hieararchies = models.PlantHierarchies.query.filter_by(plant_hierarchy_id=random_plant.plant_hierarchy_id)
+        get_parent_hieararchies = models.PlantHierarchies.query.filter_by(parent_plant_hierarchy_id=random_plant.plant_hierarchy_id)
+        return flask.render_template('site.html', family=get_family, hierarchies=get_hieararchies, parent_hierarchies=get_parent_hieararchies)
 
     return app
